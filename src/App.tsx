ec366d1802e990c5e7463f4ae926b2dd23cf977f
Lukasz Wisniewski <lukasz.wisniewski48@wp.pl>
2023-01-09 02:58:57 +0100

    creating fetch logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import NumberINput from "./components/NumericInput";
+import Table from "./components/Table";
 
-function App() {
+export interface Idata {
+  id: number;
+  name: string;
+  year: number;
+  color: string;
+  pantone_value: string;
+}
+
+const App: React.FunctionComponent = () => {
+  const per_page: number = 5;
+  const [idProducts, setIdProducts] = useState<string>("");
+  const [page, setPage] = useState<number>(1);
+  const [total, setTotal] = useState<number>(0);
+  const [products, setProducts] = useState<Idata[]>([]);
+  const [error, setError] = useState<string>("");
+
+  const fetchData = async () => {
+    try {
+      const data = await axios.get(
+        `https://reqres.in/api/{resource}?page=${page}&per_page=${per_page}'`
+      );
+      setProducts(data.data.data);
+      setPage(data.data.page);
+      setTotal(data.data.total_pages);
+      setError("");
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        setError(error.message);
+      } else {
+        throw new Error("something went wrong...");
+      }
+    }
+  };
+
+  const nextPage = () => {
+    if (total === page) {
+      return;
+    }
+    setPage((prev) => prev + 1);
+  };
+  const previousPage = () => {
+    if (page <= 1) return;
+    setPage((prev) => prev - 1);
+  };
+  useEffect(() => {
+    fetchData();
+  }, [page]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NumberINput
+        idProducts={idProducts}
+        setIdProducts={(val) => setIdProducts(val)}
+        // fetchSingleData={fetchSingleData}
+      />
+      {error ? (
+        <label style={{ color: "red" }}>{error}</label>
+      ) : (
+        <>
+          <Table products={products} />
+          <div>
+            <button
+              disabled={page === 1 ? true : false}
+              onClick={() => previousPage()}
+            >
+              {"<"}
+            </button>
+            <button
+              disabled={total === page ? true : false}
+              onClick={() => nextPage()}
+            >
+              {">"}
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
-}
+};
 
 export default App;
