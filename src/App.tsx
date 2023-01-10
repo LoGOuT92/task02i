df9ad3ff7b200896055fa4d79f458180ecf266ce
Lukasz Wisniewski <lukasz.wisniewski48@wp.pl>
2023-01-10 02:29:53 +0100

    create single product fetch

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import axios from "axios";
 import NumberINput from "./components/NumericInput";
 import Table from "./components/Table";
 
-export interface Idata {
+export interface IProduct {
   id: number;
   name: string;
   year: number;
@@ -13,22 +13,24 @@ export interface Idata {
   pantone_value: string;
 }
 
+const URL = "https://reqres.in/api/{resource}";
+
 const App: React.FunctionComponent = () => {
-  const per_page: number = 5;
-  const [idProducts, setIdProducts] = useState<string>("");
+  const itemsPerPage = 5;
+  const [filteredProductId, setFilteredProductId] = useState<string>("");
   const [page, setPage] = useState<number>(1);
-  const [total, setTotal] = useState<number>(0);
-  const [products, setProducts] = useState<Idata[]>([]);
+  const [totalPages, setTotalPages] = useState<number>(0);
+  const [products, setProducts] = useState<IProduct[]>([]);
   const [error, setError] = useState<string>("");
 
-  const fetchData = async () => {
+  const fetchProducts = async () => {
     try {
       const data = await axios.get(
-        `https://reqres.in/api/{resource}?page=${page}&per_page=${per_page}'`
+        `${URL}?page=${page}&per_page=${itemsPerPage}'`
       );
       setProducts(data.data.data);
       setPage(data.data.page);
-      setTotal(data.data.total_pages);
+      setTotalPages(data.data.total_pages);
       setError("");
     } catch (error) {
       if (axios.isAxiosError(error)) {
@@ -40,45 +42,84 @@ const App: React.FunctionComponent = () => {
   };
 
   const nextPage = () => {
-    if (total === page) {
-      return;
-    }
+    //return in last page
+    if (totalPages === page) return;
     setPage((prev) => prev + 1);
   };
   const previousPage = () => {
+    //return in first  page
     if (page <= 1) return;
     setPage((prev) => prev - 1);
   };
   useEffect(() => {
-    fetchData();
+    fetchProducts();
   }, [page]);
+
+  const fetchSinglProduct = async () => {
+    if (filteredProductId) {
+      try {
+        const data = await axios.get(`${URL}/${filteredProductId}`);
+        setProducts([data.data.data]);
+        setPage(1);
+        setTotalPages(1);
+        setError("");
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          //if the product is not in the database
+          if (error.response?.status === 404) {
+            return setError("No product found with this ID");
+          }
+          //fetch axios error
+          setError(error.message);
+        } else {
+          throw new Error("something went wrong...");
+        }
+      }
+    }
+  };
+
   return (
     <div className="App">
       <NumberINput
-        idProducts={idProducts}
-        setIdProducts={(val) => setIdProducts(val)}
-        // fetchSingleData={fetchSingleData}
+        filteredProductId={filteredProductId}
+        setFilteredProductId={(val) => setFilteredProductId(val)}
+        fetchSinglProduct={fetchSinglProduct}
+        fetchProducts={fetchProducts}
+        type="text"
+        placeholder="Find product by id"
       />
       {error ? (
-        <label style={{ color: "red" }}>{error}</label>
+        <label className="label-error">{error}</label>
       ) : (
-        <>
+        <div className="content">
           <Table products={products} />
           <div>
             <button
               disabled={page === 1 ? true : false}
               onClick={() => previousPage()}
+              className="page-button"
             >
-              {"<"}
+              <svg>
+                <path
+                  fill="currentColor"
+                  d="M20,9V15H12V19.84L4.16,12L12,4.16V9H20Z"
+                />
+              </svg>
             </button>
             <button
-              disabled={total === page ? true : false}
+              disabled={totalPages === page ? true : false}
               onClick={() => nextPage()}
+              className="page-button"
             >
-              {">"}
+              <svg>
+                <path
+                  fill="currentColor"
+                  d="M4,15V9H12V4.16L19.84,12L12,19.84V15H4Z"
+                />
+              </svg>
             </button>
           </div>
-        </>
+        </div>
       )}
     </div>
   );
