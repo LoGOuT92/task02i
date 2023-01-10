5a6cba70132c13edf6dc9e66146752783c674eeb
Lukasz Wisniewski <lukasz.wisniewski48@wp.pl>
2023-01-10 02:27:59 +0100

    refraktoring: change function and class names

---
+++ src/components/TableItem.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import { IProduct } from "../App";
+import "../App.css";
+
+interface ITableItemProps {
+  product: IProduct;
+  setIsModalOpen: (val: boolean) => void;
+  setData: (val: IProduct) => void;
+}
+
+const TableItem: React.FunctionComponent<ITableItemProps> = ({
+  product,
+  setIsModalOpen,
+  setData,
+}) => {
+  const openModal = () => {
+    setIsModalOpen(true);
+    setData(product);
+  };
+
+  return (
+    <tr
+      key={product.id}
+      style={{ backgroundColor: product.color }}
+      onClick={() => openModal()}
+      className="table--row"
+    >
+      <th>{product.id}</th>
+      <th>{product.name}</th>
+      <th>{product.year}</th>
+    </tr>
+  );
+};
+
+export default TableItem;
