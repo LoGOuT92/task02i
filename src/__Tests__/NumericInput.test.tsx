2ae62b9a7e7d7720469cbb2b55c952e3c59c8724
Lukasz Wisniewski <lukasz.wisniewski48@wp.pl>
2023-01-13 05:14:17 +0100

    Feat: Add numericInput tests

--- src/__Tests__/NumericInput.test.tsx
+++ src/__Tests__/NumericInput.test.tsx
@@ -16,3 +16,27 @@ test("renders learn react link", () => {
   const placeholderElement = screen.getByPlaceholderText(/Find product by id/i);
   expect(placeholderElement).toBeInTheDocument();
 });
+
+describe("NumberInput component", () => {
+  it("should render an input element with the correct placeholder", () => {
+    const placeholder = "Enter product ID";
+    const { getByPlaceholderText } = render(
+      <NumberINput
+        placeholder={placeholder}
+        filteredProductId={""}
+        setFilteredProductId={function (val: string): void {
+          throw new Error("Function not implemented.");
+        }}
+        fetchSinglProduct={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+        fetchProducts={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+      />
+    );
+    // eslint-disable-next-line testing-library/prefer-screen-queries
+    const input = getByPlaceholderText(placeholder);
+    expect(input).toBeInTheDocument();
+  });
+});
